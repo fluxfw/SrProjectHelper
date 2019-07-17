7a5bf18b3018a42ddf655d1bf5400c1614b55de6
Florian Wyss <fw@studer-raimann.ch>
2019-07-17 15:15:19 +0200

    Remove because fixed in core

--- src/Menu/Menu.php
+++ src/Menu/Menu.php
@@ -30,10 +30,6 @@ class Menu extends AbstractStaticPluginMainMenuProvider {
 	 * @inheritdoc
 	 */
 	public function getStaticTopItems(): array {
-		if (!self::plugin()->getPluginObject()->isActive()) {
-			return [];
-		}
-
 		return [
 			self::dic()->globalScreen()->mainmenu()->topParentItem(self::dic()->globalScreen()->identification()->plugin(self::plugin()
 				->getPluginObject(), $this)->identifier(ilSrProjectHelperPlugin::PLUGIN_ID))->withTitle(self::plugin()->translate("menu_title"))
@@ -50,10 +46,6 @@ class Menu extends AbstractStaticPluginMainMenuProvider {
 	 * @inheritdoc
 	 */
 	public function getStaticSubItems(): array {
-		if (!self::plugin()->getPluginObject()->isActive()) {
-			return [];
-		}
-
 		$parent = $this->getStaticTopItems()[0];
 
 		return [
